80138127517b03ccb64297143de58c87e1bf2ed5
shwetank <114850838+shwetank1286@users.noreply.github.com>
2023-12-20 23:16:22 +0530

    Add files via upload

---
+++ sql/Japanese Cities' Attributes.sql
@@ -0,0 +1,6 @@
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:
+
+
+select * from city where countrycode ='JPA';
